329914b941bdc984ff35820bc4e07073e96e0b81
theophilusnn <theonenhanga@gmail.com>
2026-01-16 13:13:12 -0600

    feat: basic game demo

---
+++ src/lib/index.ts
@@ -0,0 +1,233 @@
+// place files you want to import through the `$lib` alias in this folder.
+export type Tag =
+	| { type: 'brand'; value: 'aberole' | 'vivannet' | 'dossi' }
+	| { type: 'color'; value: 'red' | 'blue' | 'yellow' }
+	| { type: 'vibe'; value: 'casual' | 'sporty' | 'gothic' };
+
+export type Clothing = {
+	sprite: string;
+	defenceStyle: number;
+	attackStyle: number;
+	for: 'head' | 'chest' | 'legs';
+	id: number;
+	tag: Tag[];
+};
+
+export type Avatar = {
+	head: Clothing | null;
+	chest: Clothing | null;
+	legs: Clothing | null;
+
+	health: number;
+	defenceStyle: number;
+	attackStyle: number;
+};
+
+export type Enemy = {
+	sprite: string;
+	health: number;
+	defence: number;
+	attack: number;
+};
+
+export type Powerup = {
+	sprite: string;
+	effectTo: 'health' | 'defence' | 'attack';
+	value: number;
+	rarity: 'common' | 'uncommon' | 'rare';
+	name: string;
+};
+
+export let powerups: Powerup[] = [
+	{
+		sprite:
+			'https://cdn-media.glamira.com/media/product/newgeneration/view/1/sku/21193gisu1-round/diamond/diamond-zirconia_AAAAA/stone2/diamond-zirconia_AAAAA/stone3/diamond-zirconia_AAAAA/alloycolour/yellow.jpg',
+		effectTo: 'health',
+		value: 0.05,
+		rarity: 'common',
+		name: 'Ring of health'
+	},
+	{
+		sprite:
+			'https://static.diamondsfactory.com/image/product_v2/clrn07097/rf0122902/showcase/front/yy/di/0001.jpg',
+		effectTo: 'defence',
+		value: 0.05,
+		rarity: 'common',
+		name: 'Ring of defence'
+	},
+	{
+		sprite:
+			'https://ion.bluenile.com/sets/Jewelry/Photoshoot/Bluenile/BrioPackshot/Custom/BNS01xBNH01/BNS01xBNH01_RND/BNS01xBNH01_M1_RND_DIM_wht_0100CT_W_W/BNS01xBNH01_M1_RND_DIM_wht_0100CT_W_W.001.jpg',
+		effectTo: 'attack',
+		value: 0.05,
+		rarity: 'common',
+		name: 'Ring of attack'
+	},
+	{
+		sprite:
+			'https://media.tiffany.com/is/image/Tiffany/EcomItemL2/tiffany-1837circle-pendant-73130344_1062483_ED.jpg',
+		effectTo: 'health',
+		value: 0.1,
+		rarity: 'uncommon',
+		name: 'Amulet of health'
+	},
+	{
+		sprite: 'https://spencediamonds.com/assets/29885-A.png',
+		effectTo: 'defence',
+		value: 0.1,
+		rarity: 'uncommon',
+		name: 'Amulet of defence'
+	},
+	{
+		sprite:
+			'https://cdn-media.glamira.com/media/product/newgeneration/view/1/sku/men-yarden/diamond/browndiamond_AAA/alloycolour/yellow.jpg',
+		effectTo: 'attack',
+		value: 0.15,
+		rarity: 'rare',
+		name: 'Amulet of attack'
+	},
+	{
+		sprite:
+			'https://media.debeers.com/i/debeers/E103343_70?fmt=auto&fmt.webp.qlt=65&fmt.jp2.qlt=40&fmt.jpeg.qlt=65&$new-plp-grid-desktop-1-1-3000-poi$',
+		effectTo: 'health',
+		value: 0.1,
+		rarity: 'uncommon',
+		name: 'Earring of health'
+	},
+	{
+		sprite:
+			'https://pilgrim.ca/cdn/shop/products/262312063_33fe1723-cc6f-4992-842b-f49006127b4e-208182_x1100.jpg?v=1689598133',
+		effectTo: 'defence',
+		value: 0.15,
+		rarity: 'rare',
+		name: 'Earring of defence'
+	},
+	{
+		sprite:
+			'https://asset.swarovski.com/images/$size_1450/t_swa103/b_rgb:ffffff,c_scale,dpr_1.0,f_auto,w_375/5646733_png_var2/una-drop-earrings--round-cut--white--gold-tone-plated-swarovski-5646733.png',
+		effectTo: 'attack',
+		value: 0.05,
+		rarity: 'common',
+		name: 'Earring of attack'
+	}
+];
+
+export const extraClothes: Clothing[] = [
+	{
+		sprite:
+			'https://www.emp.co.uk/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw2e9f1013/images/5/7/0/3/570386a.jpg?sfrm=png',
+		for: 'chest',
+		defenceStyle: 8,
+		attackStyle: 2,
+		id: 3546789,
+		tag: [{ type: 'vibe', value: 'gothic' }]
+	},
+	{
+		sprite: 'https://sportyandrich.com/cdn/shop/files/SERIFLOGOSPORTSTANKNAVY2.jpg?v=1699971599',
+		for: 'chest',
+		defenceStyle: 7,
+		attackStyle: 3,
+		id: 23,
+		tag: [{ type: 'vibe', value: 'sporty' }]
+	},
+	{
+		sprite:
+			'https://dynamic-cdn.zenegal.store/fit-in/700x1050/products/55798/boys-bottom-16945867248233.jpg',
+		for: 'legs',
+		defenceStyle: 10,
+		attackStyle: 2,
+		id: 12349,
+		tag: [{ type: 'vibe', value: 'casual' }]
+	},
+	{
+		sprite:
+			'https://www.pretty-attitude.com/cdn/shop/files/gothic-bell-bottoms-4_1024x1024.jpg?v=1717243628',
+		for: 'legs',
+		defenceStyle: 9,
+		attackStyle: 3,
+		id: 1234569,
+		tag: [
+			{ type: 'vibe', value: 'gothic' },
+			{ type: 'brand', value: 'aberole' }
+		]
+	},
+	{
+		sprite:
+			'https://cdn.vectorstock.com/i/500p/44/47/vibrant-red-japanese-tengu-mask-vector-61374447.jpg',
+		for: 'head',
+		defenceStyle: 8,
+		attackStyle: 3,
+		id: 12567,
+		tag: [{ type: 'color', value: 'red' }]
+	},
+	{
+		sprite:
+			'https://sites.create-cdn.net/siteimages/5/0/3/50308/20/1/5/20157391/1000x750.jpg?1663311266',
+		for: 'head',
+		defenceStyle: 4,
+		attackStyle: 6,
+		id: 17,
+		tag: [
+			{ type: 'color', value: 'blue' },
+			{ type: 'brand', value: 'vivannet' }
+		]
+	}
+];
+
+export function newInventory(): Array<Clothing> {
+	return [
+		{
+			sprite:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTS0mNwhUWnRYBKJXxNfot5_vHy87HhptMYA&s',
+			defenceStyle: 4,
+			attackStyle: 3,
+			for: 'head',
+			id: 1,
+			tag: [
+				{ type: 'color', value: 'yellow' },
+				{ type: 'brand', value: 'dossi' }
+			]
+		},
+		{
+			sprite: 'https://tfashionmart.com/cdn/shop/products/bluedodgersfitted.jpg',
+			defenceStyle: 6,
+			attackStyle: 6,
+			for: 'head',
+			id: 2,
+			tag: [
+				{ type: 'brand', value: 'vivannet' },
+				{ type: 'color', value: 'blue' },
+				{ type: 'vibe', value: 'sporty' }
+			]
+		},
+		{
+			sprite:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Red_Hat_logo.svg/250px-Red_Hat_logo.svg.png',
+			defenceStyle: 9,
+			attackStyle: 3,
+			for: 'head',
+			id: 3,
+			tag: [
+				{ type: 'brand', value: 'aberole' },
+				{ type: 'color', value: 'red' }
+			]
+		},
+		{
+			sprite: 'https://m.media-amazon.com/images/I/51Kwn-j-8eL._AC_SL1001_.jpg',
+			defenceStyle: 6,
+			attackStyle: 6,
+			for: 'chest',
+			id: 4,
+			tag: [{ type: 'brand', value: 'dossi' }]
+		},
+		{
+			sprite:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQwJCmnST-pcu0vvRiqt2Uv0kPq8VHyBHMew&s',
+			defenceStyle: 4,
+			attackStyle: 3,
+			for: 'legs',
+			id: 5,
+			tag: [{ type: 'brand', value: 'dossi' }]
+		}
+	];
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,465 @@
+<script lang="ts">
+	import {
+		type Tag,
+		type Clothing,
+		type Avatar,
+		type Enemy,
+		type Powerup,
+		powerups,
+		extraClothes,
+		newInventory
+	} from '$lib/index';
+
+	let avatar = $state<Avatar>({
+		head: null,
+		chest: null,
+		legs: null,
+		health: 100,
+		defenceStyle: 1,
+		attackStyle: 5
+	});
+
+	let mode = $state<'pre-encounter' | 'encounter' | 'post-encounter'>('pre-encounter');
+	let win = $state<boolean>(false);
+
+	function shuffle(array: unknown[]): unknown[] {
+		let currentIndex = array.length,
+			randomIndex;
+
+		// While there remain elements to shuffle.
+		while (currentIndex !== 0) {
+			// Pick a remaining element.
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+			// And swap it with the current element using array destructuring.
+			[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+		}
+
+		return array;
+	}
+	let encounterPowerups = $state<Array<Powerup>>([]);
+	let rewardClothes = $state<Array<Clothing>>([]);
+
+	let inventory = $state<Array<Clothing>>(newInventory());
+
+	function recalculateStats() {
+		let totalDefence = 0;
+		let totalAttack = 0;
+		if (avatar.head) {
+			totalDefence += avatar.head.defenceStyle;
+			totalAttack += avatar.head.attackStyle;
+		}
+		if (avatar.chest) {
+			totalDefence += avatar.chest.defenceStyle;
+			totalAttack += avatar.chest.attackStyle;
+		}
+		if (avatar.legs) {
+			totalDefence += avatar.legs.defenceStyle;
+			totalAttack += avatar.legs.attackStyle;
+		}
+		avatar.defenceStyle = totalDefence;
+		avatar.attackStyle = totalAttack;
+	}
+
+	function pickReward(item: Clothing) {
+		inventory.push(item);
+		resetGame();
+	}
+
+	function unequipItem(item: Clothing) {
+		if (item.for === 'head') {
+			avatar.head = null;
+		} else if (item.for === 'chest') {
+			avatar.chest = null;
+		} else if (item.for === 'legs') {
+			avatar.legs = null;
+		}
+		recalculateStats();
+	}
+
+	function equipItem(item: Clothing) {
+		if (item.for === 'head') {
+			if (avatar.head) {
+				unequipItem(avatar.head);
+			}
+			avatar.head = item;
+		} else if (item.for === 'chest') {
+			if (avatar.chest) {
+				unequipItem(avatar.chest);
+			}
+			avatar.chest = item;
+		} else if (item.for === 'legs') {
+			if (avatar.legs) {
+				unequipItem(avatar.legs);
+			}
+			avatar.legs = item;
+		}
+		recalculateStats();
+	}
+
+	let endMsg = $state('');
+	let rollDieVid = $state(false);
+	let yourMultiplier = $state(1);
+	let enemyMultiplier = $state(1);
+	let yourDie = $state(0);
+	let enemyDie = $state(0);
+	let yourDamage = $state(0);
+	let enemyDamage = $state(0);
+
+	let enemy = $state<Enemy>({
+		sprite:
+			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK3KPjPwcINGG6hMNDOANfvmTgIAQ1vrOTnw&s',
+		defence: 15,
+		attack: 15,
+		health: 100
+	});
+
+	let turnNumber = $state(0);
+
+	function resetGame(): void {
+		if (!win) {
+			inventory = newInventory();
+		}
+		endMsg = '';
+		rollDieVid = false;
+		yourMultiplier = 1;
+		enemyMultiplier = 1;
+		yourDie = 0;
+		enemyDie = 0;
+		yourDamage = 0;
+		enemyDamage = 0;
+
+		enemy.health = 100;
+		enemy.defence = Math.floor(Math.random() * 10) + 15;
+		enemy.attack = Math.floor(Math.random() * 10) + 15;
+
+		avatar.health = 100;
+		avatar.defenceStyle = 1;
+		avatar.attackStyle = 5;
+		avatar.head = null;
+		avatar.chest = null;
+		avatar.legs = null;
+
+		turnNumber = 0;
+		mode = 'pre-encounter';
+	}
+
+	function pickRewards() {
+		rewardClothes = [];
+		let nonOwnedItems = extraClothes.filter((item) => !inventory.includes(item));
+		shuffle(nonOwnedItems);
+		for (let i = 0; i < Math.min(3, nonOwnedItems.length); i++) {
+			rewardClothes.push(nonOwnedItems[i]);
+		}
+	}
+
+	async function attack(avatar: Avatar, target: Enemy) {
+		rollDieVid = true;
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+		rollDieVid = false;
+
+		yourDie = Math.ceil(Math.random() * 6);
+		enemyDie = Math.ceil(Math.random() * 6);
+
+		yourMultiplier = 1 + yourDie / 10;
+		enemyMultiplier = 1 + enemyDie / 10;
+
+		yourDamage = ((100 - target.defence) / 100) * (avatar.attackStyle * yourMultiplier);
+		enemyDamage = ((100 - avatar.defenceStyle) / 100) * (target.attack * enemyMultiplier);
+
+		yourDamage = Math.floor(yourDamage);
+		enemyDamage = Math.floor(enemyDamage);
+
+		target.health -= yourDamage;
+		if (target.health <= 0) {
+			target.health = 0;
+			endMsg = 'You defeated the enemy!';
+			win = true;
+			mode = 'post-encounter';
+			pickRewards();
+		}
+
+		avatar.health -= enemyDamage;
+		if (avatar.health <= 0) {
+			avatar.health = 0;
+			endMsg = 'You were defeated!';
+			win = false;
+			mode = 'post-encounter';
+		}
+		turnNumber++;
+	}
+
+	function toEncounterMode(): void {
+		encounterPowerups = [];
+		mode = 'encounter';
+		shuffle(powerups);
+		for (let i = 0; i < 3; i++) {
+			encounterPowerups.push(powerups[i]);
+		}
+	}
+
+	function usePowerup(powerup: Powerup): void {
+		switch (powerup.effectTo) {
+			case 'health':
+				avatar.health *= 1 + powerup.value;
+				avatar.health = Math.round(avatar.health);
+				break;
+			case 'attack':
+				avatar.attackStyle *= 1 + powerup.value;
+				avatar.attackStyle = Math.round(avatar.attackStyle);
+				break;
+			case 'defence':
+				avatar.defenceStyle *= 1 + powerup.value;
+				avatar.defenceStyle = Math.round(avatar.defenceStyle);
+				break;
+		}
+		encounterPowerups = encounterPowerups.filter((p) => p !== powerup);
+	}
+</script>
+
+{#snippet player()}
+	<section class="flex w-fit flex-col gap-2 rounded-md border p-2">
+		<h3 class="text-xl font-bold">Player</h3>
+		<div class="flex flex-col">
+			<p>Head</p>
+			{#if avatar.head}
+				<img class="h-12 w-12" src={avatar.head.sprite} alt="Head" />
+			{:else}
+				<p>No clothing worn here.</p>
+			{/if}
+		</div>
+		<div class="flex flex-col">
+			<p>Chest</p>
+			{#if avatar.chest}
+				<img class="h-12 w-12" src={avatar.chest.sprite} alt="Chest" />
+			{:else}
+				<p>No clothing worn here.</p>
+			{/if}
+		</div>
+		<div class="flex flex-col">
+			<p>Legs</p>
+			{#if avatar.legs}
+				<img class="h-12 w-12" src={avatar.legs.sprite} alt="Legs" />
+			{:else}
+				<p>No clothing worn here.</p>
+			{/if}
+		</div>
+
+		<div class="w-full border-t">
+			<div>
+				<p class="flex justify-between gap-4 font-bold">Health: <span>{avatar.health}</span></p>
+				<p class="flex justify-between gap-4 font-bold">
+					Defence Style: <span>{avatar.defenceStyle}</span>
+				</p>
+				<p class="flex justify-between gap-4 font-bold">
+					Attack Style: <span>{avatar.attackStyle}</span>
+				</p>
+			</div>
+		</div>
+	</section>
+{/snippet}
+
+{#snippet enemyMarkup()}
+	<section class="flex flex-col gap-2 rounded-md border p-2">
+		<h3 class="text-xl font-bold">Enemy</h3>
+		<img class="h-24 w-24" src={enemy.sprite} alt="enemy sprite" />
+		<div class="w-fit">
+			<h2>Enemy Stats</h2>
+			<div>
+				<p class="font-bold">health: {enemy.health}</p>
+				<p class="font-bold">defence: {enemy.defence}</p>
+				<p class="font-bold">attack: {enemy.attack}</p>
+			</div>
+		</div>
+	</section>
+{/snippet}
+
+<div class="relative m-4 flex flex-col space-y-4">
+	<div class="absolute top-0 right-0">
+		<p class="text-muted-foreground font-serif text-5xl font-extrabold">{mode}</p>
+	</div>
+	{#if mode === 'pre-encounter'}
+		{@render player()}
+
+		<div class="flex flex-col gap-2">
+			<h2 class="text-2xl font-semibold">Clothes Inventory</h2>
+			<div class="flex flex-col gap-2">
+				<p>Head armour</p>
+				<div class="flex gap-2">
+					{#each inventory.filter((item) => item.for === 'head') as item, index (index)}
+						<button
+							onclick={() => equipItem(item)}
+							class="w-fit border border-black p-2 text-left shadow hover:shadow-md"
+						>
+							<img class="h-12 w-12" src={item.sprite} alt={item.for} />
+							<p class="flex justify-between gap-4">
+								defence Style: <span>{item.defenceStyle}</span>
+							</p>
+							<p class="flex justify-between">attack style: <span>{item.attackStyle}</span></p>
+						</button>
+					{/each}
+				</div>
+			</div>
+			<div class="flex flex-col gap-2">
+				<p>Chest armour</p>
+				<div class="flex gap-2">
+					{#each inventory.filter((item) => item.for === 'chest') as item, index (index)}
+						<button
+							onclick={() => equipItem(item)}
+							class="w-fit border border-black p-2 shadow hover:shadow-md"
+						>
+							<img class="h-12 w-12" src={item.sprite} alt={item.for} />
+							<p class="flex justify-between gap-4">
+								defence Style: <span>{item.defenceStyle}</span>
+							</p>
+							<p class="flex justify-between">attack style: <span>{item.attackStyle}</span></p>
+						</button>
+					{/each}
+				</div>
+			</div>
+			<div class="flex flex-col gap-2">
+				<p>Legs armour</p>
+				<div class="flex gap-2">
+					{#each inventory.filter((item) => item.for === 'legs') as item, index (index)}
+						<button
+							onclick={() => equipItem(item)}
+							class="w-fit border border-black p-2 shadow hover:shadow-md"
+						>
+							<img class="h-12 w-12" src={item.sprite} alt={item.for} />
+							<p class="flex justify-between gap-4">
+								defence Style: <span>{item.defenceStyle}</span>
+							</p>
+							<p class="flex justify-between">attack style: <span>{item.attackStyle}</span></p>
+						</button>
+					{/each}
+				</div>
+			</div>
+			<button
+				onclick={toEncounterMode}
+				class="mt-2 ml-2 w-fit border px-3 py-1 text-xl font-semibold hover:shadow"
+				>Ready to battle with style!</button
+			>
+		</div>
+	{:else if mode === 'encounter'}
+		<div class="flex flex-col space-y-6">
+			<div class="flex items-center gap-6">
+				{@render player()}
+				<p class="text-5xl font-bold">VS</p>
+				{@render enemyMarkup()}
+			</div>
+
+			<div class="flex gap-4">
+				{#each encounterPowerups as powerup, index (index)}
+					<div class="flex flex-col items-center">
+						<button
+							onclick={() => usePowerup(powerup)}
+							class=" overflow-hidden rounded-full border-2 shadow hover:shadow-md {powerup.rarity ===
+							'common'
+								? 'border-green-500'
+								: powerup.rarity === 'uncommon'
+									? 'border-blue-500'
+									: powerup.rarity === 'rare'
+										? 'border-purple-500'
+										: 'border-gray-500'}"
+						>
+							<img class="h-24 w-24" src={powerup.sprite} alt={powerup.name} />
+						</button>
+						<p>{powerup.name}</p>
+					</div>
+				{/each}
+			</div>
+
+			<div class="flex flex-col gap-4">
+				<p>Turn: {turnNumber}</p>
+				<button
+					onclick={() => attack(avatar, enemy)}
+					class=" w-fit border border-black px-3 py-1 shadow hover:shadow-md">Attack</button
+				>
+				{#if rollDieVid === true}
+					<div class="flex flex-col items-center justify-center">
+						<p>Rolling die...</p>
+						<img
+							src="https://i.pinimg.com/originals/75/09/82/750982c4ccc4737e643207f54c40170a.gif"
+							alt="Rolling die"
+							class="h-24 w-24"
+						/>
+					</div>
+				{/if}
+
+				<div class="flex flex-col gap-4">
+					<div>
+						<p>Your roll: {yourDie}</p>
+						<p>Enemy roll: {enemyDie}</p>
+					</div>
+
+					<div>
+						<p>Your multiplier: {yourMultiplier}</p>
+						<p>Enemy multiplier: {enemyMultiplier}</p>
+					</div>
+					<div>
+						<p>Your damage: {yourDamage}</p>
+						<p>Enemy damage: {enemyDamage}</p>
+					</div>
+
+					<div class="text-muted-foreground space-y-4">
+						<div>
+							<p>Damage formula:</p>
+							<p>((100 - target.defence) / 100) * (avatar.attack * yourMultiplier)</p>
+						</div>
+						<div>
+							<p>Powerup Effect:</p>
+							<p>avatar.[health|attack|defence] *= 1 + powerup.value;</p>
+							<small>powerup values are between 0 and 1</small>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	{:else if mode === 'post-encounter'}
+		<div class="flex flex-col gap-4">
+			{#if win}
+				<p>{endMsg}</p>
+
+				<div>
+					<div>
+						{#if rewardClothes.length !== 0}
+							<h3 class="text-xl font-bold">Pick a Reward</h3>
+							{#each rewardClothes as reward (reward.id)}
+								<button
+									onclick={() => {
+										pickReward(reward);
+									}}
+									class=" w-fit border border-black px-3 py-1 shadow hover:shadow-md"
+								>
+									<img src={reward.sprite} alt="a piece of clothing" class="h-24 w-24" />
+								</button>
+							{/each}
+						{:else}
+							<p>No rewards available</p>
+						{/if}
+					</div>
+				</div>
+			{:else}
+				<p>{endMsg}</p>
+			{/if}
+
+			<button
+				onclick={() => {
+					resetGame();
+				}}
+				class="w-fit border border-black px-3 py-1 shadow hover:shadow-md">Play Again</button
+			>
+		</div>
+	{/if}
+</div>
+
+<style>
+	button {
+		@apply transition-all duration-150 active:scale-90;
+		border-radius: 1rem;
+		cursor: pointer;
+	}
+	span {
+		font-weight: bold;
+	}
+</style>
